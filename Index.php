bdd3dafdd3d95ec6e982676aa341a9786180f596
Abou-Shadadi <naslab512@gmail.com>
2021-08-07 10:57:28 +0200

    Making some chages to make sure we can start

--- Index.php
+++ Index.php
@@ -1,5 +1,26 @@
     <!--*************___Including-Head parts___**************-->
-    <?php include 'Shared/Head.php';?>
+    <?php 
+    // login session
+    
+    if(isset($_POST['login'])){
+//
+
+
+//
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // end of loggin session
+    include 'Shared/Head.php';?>
     <title>SignIN - TraverIS</title>
   </head>
   <body>
@@ -31,9 +52,9 @@
               </h1>
               <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Email or Username:</span>
-                <input
+                <input type="email"
                   class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                  placeholder="Jane Doe"
+                  placeholder="me@mail.com"  required name="email"
                 />
               </label>
               <label class="block mt-4 text-sm">
@@ -41,14 +62,15 @@
                 <input
                   class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                   placeholder="***************"
-                  type="password"
+                  type="password" name="password"
                 />
               </label>
-              <button
+              <button type="submit"
                 class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white 
                 transition-colors duration-150 bg-purple-600 border 
                 border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 
                 focus:outline-none focus:shadow-outline-purple"
+                name="login"
               >
                 SignIN
               </button>
